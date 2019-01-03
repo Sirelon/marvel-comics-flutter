4c673f63e2cc54a9fa2f613ea0b5eeb8046c0a1c
Alexandr Romanishin <alexandr.romanishin@tbwamobile.com>
2019-01-03 16:26:46 +0200

    Get color from image, set it to AppBar and Texts

--- lib/SensitiveWidget.dart
+++ lib/SensitiveWidget.dart
@@ -61,15 +61,15 @@ class _SensitiveWidgetState extends State<SensitiveWidget> {
 
   @override
   Widget build(BuildContext context) {
-    rotX *= 0.53;
-    rotY *= 0.53;
+    rotX *= 0.43;
+    rotY *= 0.43;
 
     var transform = Transform(
       transform: perspective
         ..setRotationX(rotX)
         ..rotateY(rotY),
       alignment: FractionalOffset.center,
-      child: Transform.scale(scale: 1.2, child: widget.child),
+      child: Transform.scale(scale: 1.3, child: widget.child),
     );
 
     return transform;
--- lib/detail.dart
+++ lib/detail.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:marvel_heroes/entities.dart';
 import 'package:marvel_heroes/SensitiveWidget.dart';
+import 'package:palette_generator/palette_generator.dart';
+import 'package:random_color/random_color.dart';
 
 class HeroDetailPage extends StatefulWidget {
   final MarvelHero hero;
@@ -13,44 +15,100 @@ class HeroDetailPage extends StatefulWidget {
 }
 
 class _HeroDetailPageState extends State<HeroDetailPage> {
+  ImageProvider imageProvider;
+
+  PaletteGenerator paletteGenerator;
+  Color dominantColor = Colors.white;
+  Color invertColor = Colors.white;
+  RandomColor _randomColor = RandomColor();
+
+  @override
+  void initState() {
+    imageProvider = CachedNetworkImageProvider(widget.hero.image);
+    _updatePaletteGenerator();
+    super.initState();
+  }
+
+  Future<void> _updatePaletteGenerator() async {
+    PaletteGenerator.fromImageProvider(imageProvider).then((value) {
+      setState(() {
+        paletteGenerator = value;
+        dominantColor = paletteGenerator.dominantColor?.color;
+        print('Dominant color ' + dominantColor.toString());
+        invertColor = Color.fromARGB(
+            dominantColor.alpha,
+            255 - dominantColor.red,
+            255 - dominantColor.green,
+            255 - dominantColor.blue);
+//        dominantColor = _randomColor.randomColor(colorSaturation: ColorSaturation.highSaturation);
+        print('Invert color ' + dominantColor.toString());
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final hero = widget.hero;
-    final bg = CachedNetworkImage(
-        errorWidget: Icon(Icons.error),
-        imageUrl: hero.image,
+//    final bg = CachedNetworkImage(
+//        errorWidget: Icon(Icons.error),
+//        imageUrl: hero.image,
+//        fit: BoxFit.cover,
+//        height: double.infinity,
+//        width: double.infinity,
+//        alignment: Alignment.center,
+//        placeholder: CircularProgressIndicator());
+
+    final bg = Image(
+        image: imageProvider,
         fit: BoxFit.cover,
         height: double.infinity,
         width: double.infinity,
-        alignment: Alignment.center,
-        placeholder: CircularProgressIndicator());
+        alignment: Alignment.center);
+
+    final iconAppBarTheme = IconThemeData(color: invertColor);
 
     return new Scaffold(
-        appBar: AppBar(title: Text(widget.hero.name)),
+        appBar: AppBar(
+          title: Text(
+            widget.hero.name,
+            style: TextStyle(color: invertColor),
+          ),
+          backgroundColor: dominantColor,
+          brightness: ThemeData.estimateBrightnessForColor(dominantColor),
+          iconTheme: iconAppBarTheme,
+        ),
         body: new Stack(children: <Widget>[
           SensitiveWidget(child: bg),
-          SafeArea(child: DetailHeroInfo(hero: hero))
+          SafeArea(
+              child: DetailHeroInfo(
+            hero: hero,
+            titleColor: invertColor,
+          ))
         ]));
   }
 }
 
 class DetailHeroInfo extends StatelessWidget {
-  const DetailHeroInfo({
-    Key key,
-    @required this.hero,
-  }) : super(key: key);
+  const DetailHeroInfo(
+      {Key key, @required this.hero, @required this.titleColor})
+      : super(key: key);
 
   final MarvelHero hero;
+  final Color titleColor;
 
   @override
   Widget build(BuildContext context) {
     var padding = 8.0;
-    var blackStyle = TextStyle(
+    var titleStyle = TextStyle(
         fontFamily: 'Black',
-        color: Colors.white,
-        fontWeight: FontWeight.normal,
+        color: titleColor,
+        fontWeight: FontWeight.w800,
         letterSpacing: padding,
         fontSize: 42.0);
+    var subTitleStyle = TextStyle(
+      fontSize: 24.0,
+      color: titleColor,
+    );
 
     return Center(
         child: Column(
@@ -58,7 +116,7 @@ class DetailHeroInfo extends StatelessWidget {
       children: <Widget>[
         Text(
           hero.name,
-          style: blackStyle,
+          style: titleStyle,
           textAlign: TextAlign.center,
         ),
         SizedBox(
@@ -68,7 +126,7 @@ class DetailHeroInfo extends StatelessWidget {
             padding: EdgeInsets.all(padding),
             child: Text(
               '     ${hero.description}',
-              style: Theme.of(context).primaryTextTheme.title,
+              style: subTitleStyle,
               textAlign: TextAlign.start,
             )),
       ],
