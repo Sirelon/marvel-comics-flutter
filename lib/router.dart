563679c3b143c56e5d60af43b9a03b382d649136
Alexandr Romanishin <alexandr.romanishin@tbwamobile.com>
2019-04-30 22:16:35 +0300

    "Hacky" deal with long loading when openning detail hero page.

--- lib/router.dart
+++ lib/router.dart
@@ -1,16 +1,37 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' as prefix0;
 import 'package:marvel_heroes/hero/detail/detail.dart';
 import 'package:marvel_heroes/entities.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:marvel_heroes/widgets/loading_widgets.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 class Router {
   final BuildContext context;
 
   Router(this.context);
 
-  void navigateToHero(MarvelHero hero) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (c) => HeroDetailPage(hero: hero)));
+  void navigateToHero(MarvelHero hero) async {
+    final dialogFeture = showDialog(
+        context: context,
+        builder: (BuildContext context) =>
+            AlertDialog(content: SmallLoadingWidget()));
+
+    try {
+      // For "warm" our pallete generator.
+      await PaletteGenerator.fromImageProvider(
+              CachedNetworkImageProvider(hero.urlHolder.detailUrl))
+          .timeout(Duration(seconds: 2));
+    } catch (e) {
+      print(e);
+    } finally {
+      // Hide Dialog
+      Navigator.pop(context);
+      Navigator.push(context,
+          MaterialPageRoute(builder: (c) => HeroDetailPage(hero: hero)));
+    }
   }
 
   void closeCurrent() {
@@ -27,14 +48,14 @@ class Router {
           enableUrlBarHiding: true,
           showPageTitle: true,
           animation: new CustomTabsAnimation.slideIn()
-              // or user defined animation.
+          // or user defined animation.
 //          animation: new CustomTabsAnimation(
 //          startEnter: 'slide_up',
 //          startExit: 'android:anim/fade_out',
 //          endEnter: 'android:anim/fade_in',
 //          endExit: 'slide_down',
 //        )
-              ,
+          ,
           extraCustomTabs: <String>[
             // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
             'org.mozilla.firefox',
